e143068cbc0b98c098222f0cc67cd2e7d12755b3
Bruce Wallin <bruce.wallin@spire.com>
2023-03-19 15:07:26 -0600

    fix: Activate conda env in create-env.sh script

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -4,5 +4,6 @@ THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${THIS_DIR}/common.sh"
 
 conda create --name "$PROJECT_NAME" python=3.10 pip
+conda activate "$PROJECT_NAME"
 pip install -r "${SRC_DIR}/requirements.txt"
 pip install -r "${SRC_DIR}/requirements.dev"
